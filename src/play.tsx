2b61cc80617d18105230441c15a42a5795d9c28b
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-07-19 22:31:56 +0300

    rename ref and resize canvas

--- src/play.tsx
+++ src/play.tsx
@@ -4,7 +4,7 @@ import { setupCanvas } from './canvas';
 import pythonCode from './samples/play.py';
 
 export const Play = () => {
-  const ref = useRef(null);
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     let anim = null;
@@ -19,13 +19,13 @@ export const Play = () => {
         '/otto-simulator/ottosim-0.1.0-cp311-cp311-emscripten_3_1_32_wasm32.whl',
       ]);
 
-      const gl = ref.current.getContext('webgl2', {
+      const gl = canvasRef.current.getContext('webgl2', {
         alpha: false, depth: false, stencil: false, antialias: false,
         premultipliedAlpha: false, preserveDrawingBuffer: false,
         powerPreference: 'high-performance',
       });
 
-      uninstallEventHandlers = setupCanvas(pyodide, ref.current, gl);
+      uninstallEventHandlers = setupCanvas(pyodide, canvasRef.current, gl);
 
       pyodide.runPython(pythonCode);
       const updateCallback = pyodide.globals.get('update');
@@ -55,9 +55,9 @@ export const Play = () => {
       }}
     >
       <canvas
-        ref={ref}
-        width={1280}
-        height={720}
+        ref={canvasRef}
+        width={1920}
+        height={1080}
         style={{
           width: '100vw',
           height: '100vh',
